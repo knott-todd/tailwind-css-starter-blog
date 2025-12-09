336a6a8828c7a46d86f811ae636bcc379eb4b72c
knott-todd <olusanya.nkt.todd@gmail.com>
2025-12-08 23:38:01 -0500

    style: :lipstick: edit project page text

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Check out my latest work
           </p>
         </div>
         <div className="container py-12">
